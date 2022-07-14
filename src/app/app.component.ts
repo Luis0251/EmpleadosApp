47e2abd893c535b88229c94eb83b1b31cee4bc1d
Luis0251 <luis0251@hotmail.com>
2022-07-14 09:22:19 -0500

    Finalizacion de la App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleados } from './empleados.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmpleadosApp';
+  empleados:Empleados[] = [
+    new Empleados("jua","carlos","CEO",50000),
+    new Empleados("jua","carlos","Director",40000),
+    new Empleados("jua","carlos","Admin",30000),
+    new Empleados("jua","carlos","Jefe de seccion",60000),
+  ];
+  registrar(){
+    let miEmpleados = new Empleados(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleados);
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+
 }
