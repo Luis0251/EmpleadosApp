9bc414a17adfe2954298b5f36daf5014b66a065e
Luis0251 <luis0251@hotmail.com>
2022-07-18 16:07:37 -0500

    Rutas segunda parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Empleados } from './empleados.model';
-import { DataService } from './empleados.service';
-import { ServicioEmpleadoService } from './servicio-empleado.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,23 +6,8 @@ import { ServicioEmpleadoService } from './servicio-empleado.service';
 })
 export class AppComponent implements OnInit {
   title = 'EmpleadosApp';
-
-  constructor(/*private miServicio: ServicioEmpleadoService,*/ private dataService: DataService) {
+  constructor(/*private miServicio: ServicioEmpleadoService,*/ ) {
   }
   ngOnInit(): void {
-    this.empleados = this.dataService.empleados;
   }
-  empleados:Empleados[] = [];
-  registrar(){
-    let miEmpleados = new Empleados(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
-    //this.miServicio.messageService("el nombre del empleado es" + miEmpleados.nombre)
-    this.dataService.agregarEm(miEmpleados);
-  }
-
-  cuadroNombre:string="";
-  cuadroApellido:string="";
-  cuadroCargo:string="";
-  cuadroSalario:number=0;
-
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleados } from '../empleados.model';
+import { DataService } from '../empleados.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  title = 'EmpleadosApp';
+  constructor(private dataService: DataService) { }
+  empleados:Empleados[] = [];
   ngOnInit(): void {
+    this.empleados = this.dataService.empleados;
   }
-
+  registrar(){
+    let miEmpleados = new Empleados(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    //this.miServicio.messageService("el nombre del empleado es" + miEmpleados.nombre)
+    this.dataService.agregarEm(miEmpleados);
+  }
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 }
