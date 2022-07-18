32b18390590424cc9cea9978ecfaa2495e9250f0
Luis0251 <luis0251@hotmail.com>
2022-07-18 15:27:03 -0500

    Creacion de nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { ListEmpleadoComponent } from './list-empleado/list-empleado.component';
 import { CaracterEmplComponent } from './caracter-empl/caracter-empl.component';
 import { ServicioEmpleadoService } from './servicio-empleado.service';
+import { DataService } from './empleados.service';
+import { HomeComponent } from './home/home.component';
+import { QuienSomosComponent } from './quien-somos/quien-somos.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmpleadoComponent,
-    CaracterEmplComponent
+    CaracterEmplComponent,
+    HomeComponent,
+    QuienSomosComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,FormsModule
   ],
-  providers: [ServicioEmpleadoService],
+  providers: [ServicioEmpleadoService,DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
